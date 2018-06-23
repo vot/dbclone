c65ae85adfe91e2ac9fda0c29e38bba64fe98512
Vot <vot@siliconfen.co>
2018-06-23 02:59:24 +0100

    Functional import

--- cli.js
+++ cli.js
@@ -1,36 +1,55 @@
+#!/usr/bin/env node
 const prompt = require('prompt');
 const importRoutine = require('./lib/import');
 const exportRoutine = require('./lib/export');
+const resolveDir = require('./lib/utils/resolveDir');
 
 const schema = {
   properties: {
     mode: {
       pattern: /^(import|export)$/,
-      message: 'Mode must be "import" or "export".',
+      type: 'string',
+      message: 'Mode must equal "import" or "export".',
       required: true
     },
     host: {
       pattern: /^[a-zA-Z0-9.,:-]+$/,
-      message: 'Mongo host must be provided.',
-      required: true
+      type: 'string',
+      message: 'A valid host must be provided.',
+      default: 'localhost'
     },
     db: {
       pattern: /^[a-zA-Z0-9.,:-]+$/,
-      message: 'Mongo database name must be provided.',
+      type: 'string',
+      message: 'A valid Mongo database name must be provided.',
       required: true
     },
     dataDir: {
-      // hidden: true
+      type: 'string',
+      default: 'data'
     }
+    // username: {
+    //   type: 'string'
+    // },
+    // password: {
+    //   type: 'string',
+    //   hidden: true
+    // },
+    // include: {
+    //   type: 'string'
+    // },
+    // exclude: {
+    //   type: 'string'
+    // }
   }
 };
 
 function main() {
   console.log('dbclone');
   console.log('---------------------\n');
-  console.log('Supported modes:   "import" / "export"');
-  console.log('MongoURL format:   "mongodb://localhost/database"');
-  console.log('Default data dir:  "data"');
+  console.log('Supported modes:   import, export');
+  console.log('Default host:      localhost');
+  console.log(`Default data dir:  ${resolveDir('data')}`);
   console.log('\n---------------------\n');
 
   prompt.start();
