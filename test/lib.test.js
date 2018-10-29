7b09f5df2de6a40100696e05c712190ce9e3f959
Anish Karandikar <anishkny@gmail.com>
2018-10-29 18:51:52 -0400

    test(export): Test export from mLab URL

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,8 +1,35 @@
-const dbclone = require('../lib');
+require('dotenv').config();
 
-describe('Library tests', () => {
-  it('should import data', async () => {
-    console.log(dbclone);
-    // TODO: Add actual test
+const dbclone = require('../lib');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+
+const DATADIR = `data/${Math.random().toString(36).slice(2)}`;
+const TEST_MLAB_URL = process.env.TEST_MLAB_URL;
+const getDBNameFromURL = URL => URL.slice(URL.lastIndexOf('/') + 1);
+
+before(() => {
+  if (!TEST_MLAB_URL) {
+    throw new Error('TEST_MLAB_URL env var must be specified');
+  }
+  rimraf.sync('data');
+  mkdirp.sync(DATADIR);
+});
+
+describe('Library tests', async () => {
+  describe('Export', async () => {
+    it('should export data from mLab URL', async () => {
+      await new Promise((resolve) => {
+        dbclone.export({
+          host: TEST_MLAB_URL,
+          db: getDBNameFromURL(TEST_MLAB_URL),
+          dataDir: DATADIR,
+        }, (e, d) => {
+          console.log(`e = [${e}]`);
+          console.log(`d = [${d}]`);
+          resolve();
+        });
+      });
+    });
   });
 });
